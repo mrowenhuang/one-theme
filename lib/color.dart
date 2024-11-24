7c60d466308b91a20c5ee55a04f6abad9705a805
Owen Huang <113186362+mrowenhuang@users.noreply.github.com>
2024-11-24 11:06:20 +0700

    create default color

---
+++ lib/color.dart
@@ -0,0 +1,36 @@
+class MyColor {
+  // & Hightlight
+  static Color hightlightDarkest = Color(0xff006FFD);
+  static Color hightlightDark = Color(0xff2897FF);
+  static Color hightlightMedium = Color(0xff6FB9FF);
+  static Color hightlightLight = Color(0xffB3DAFF);
+  static Color hightlightLightest = Color(0xffEAF2FF);
+
+// & Neutral
+// ! light
+  static Color neutralLightDarkest = Color(0xffC5C6CC);
+  static Color neutralLightDark = Color(0xffD4D6DD);
+  static Color neutralLightMedium = Color(0xffE8E9F1);
+  static Color neutralLightLight = Color(0xffF8F9FE);
+  static Color neutralLightLightest = Color(0xffFFFFFF);
+// ! dark
+  static Color neutralDarkDarkest = Color(0xff1E1E1E);
+  static Color neutralDarkDark = Color(0xff2F3036);
+  static Color neutralDarkMedium = Color(0xff494A50);
+  static Color neutralDarkLight = Color(0xff71727A);
+  static Color neutralDarkLightest = Color(0xff8F9098);
+
+// & Support
+// ! succes
+  static Color supportSuccesDark = Color(0xff298267);
+  static Color supportSuccesMedium = Color(0xff3AC0A0);
+  static Color supportSuccesLight = Color(0xffE7F4E8);
+// ! warning
+  static Color supportWarningDark = Color(0xffE86339);
+  static Color supportWarningMedium = Color(0xffFFB37C);
+  static Color supportWarningLight = Color(0xffFFF4E4);
+// ! error
+  static Color supportErrorDark = Color(0xffED3241);
+  static Color supportErrorMedium = Color(0xffFF616D);
+  static Color supportErrorLight = Color(0xffFFE2E5);
+}
